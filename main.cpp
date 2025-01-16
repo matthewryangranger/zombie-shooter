48cd82641cc73704220dba2f537d9b00492535c8
Matthew Ryan Granger <mattg1827@gmail.com>
2025-01-16 13:41:16 -0800

    Create main game loop

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "Player.h"
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+  enum class State {PAUSED, LEVELING_UP, GAME_OVER, PLAYING};
+
+  auto state = State::GAME_OVER;
+  sf::Vector2f resolution;
+  resolution.x = sf::VideoMode::getDesktopMode().width;
+  resolution.y = sf::VideoMode::getDesktopMode().height;
+
+  sf::RenderWindow window(sf::VideoMode(resolution.x, resolution.y), "Zombie Shooter", sf::Style::Fullscreen);
+  sf::View mainView(sf::FloatRect(0, 0, resolution.x, resolution.y));
+  sf::Clock clock;
+  sf::Time gameTimeTotal;
+  sf::Vector2f mouseWorldPosition;
+  sf::Vector2i mouseScreenPosition;
+  Player player;
+  sf::IntRect arena;
+
+  while (window.isOpen())
+  {
+
+  }
+  return 0;
 }
